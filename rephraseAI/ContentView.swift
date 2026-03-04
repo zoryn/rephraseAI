b8a17c11097507f1fa5f301f14068a1f0eb5682c
Konstantin Zoryn <kanstan@amazon.com>
2026-03-03 20:03:55 -0800

    Initial Commit

---
+++ rephraseAI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  rephraseAI
+//
+//  Created by Zoryn, Konstantin on 3/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ rephraseAI/rephraseAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  rephraseAIApp.swift
+//  rephraseAI
+//
+//  Created by Zoryn, Konstantin on 3/3/26.
+//
+
+import SwiftUI
+
+@main
+struct rephraseAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
